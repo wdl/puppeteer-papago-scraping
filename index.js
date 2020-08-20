9b7743bc5df0274fc3d53a1c5033bcd04aa1a788
Elenchus <sijongyeoil@gmail.com>
2020-08-20 18:26:20 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+const htmlToText = require('html-to-text');
+
+module.exports = async (sk, tk, text) => {
+    const textEncoded = encodeURI(text)
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://papago.naver.com/?sk=${sk}&tk=${tk}&st=${textEncoded}`)
+    await page.waitForSelector('#txtTarget')
+    await page.waitForFunction('document.getElementById("txtTarget").innerHTML')
+    const translatedHtml = await page.evaluate('document.getElementById("txtTarget").innerHTML')
+    await browser.close();
+    return htmlToText.fromString(translatedHtml)
+}
