fd1a3b734e1ad4f7c04fa29e9b4ea44ba5e8c5e4
Elenchus <sijongyeoil@gmail.com>
2021-07-15 00:58:50 +0900

    Improve error handling

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const puppeteer = require('puppeteer');
-const htmlToText = require('html-to-text');
+'use strict';
 
-module.exports = async (sk, tk, text) => {
-    const textEncoded = encodeURIComponent(text.replace(/&/g, '%amp'))
-    const browser = await puppeteer.launch();
+const puppeteer = require('puppeteer');
+
+async function puppeteerPapagoScraping(text, to, from = 'auto') {
+  let browser;
+  try {
+    const textEncoded = encodeURIComponent(text.replace(/&/g, '%amp'));
+    browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(`https://papago.naver.com/?sk=${sk}&tk=${tk}&st=${textEncoded}`)
-    await page.waitForSelector('#txtTarget')
-    await page.waitForFunction('document.getElementById("txtTarget").innerHTML')
-    const translatedHtml = await page.evaluate('document.getElementById("txtTarget").innerHTML')
+    if (textEncoded.length > 1200) {
+      await page.goto(`https://papago.naver.com/?sk=${from}&tk=${to}`);
+      await page.waitForSelector('#txtSource');
+      await page.waitForFunction('document.getElementById("txtSource").value = `' + text.replace(/`/g, '\\`') + '`');
+      await page.type('#txtSource', String.fromCharCode(13));
+      await page.click('#btnTranslate');
+    } else {
+      await page.goto(`https://papago.naver.com/?sk=${from}&tk=${to}&st=${textEncoded}`);
+    }
+    await page.waitForSelector('#txtTarget');
+    await page.waitForFunction('document.getElementById("txtTarget").innerHTML');
+    await page.waitForFunction('document.getElementById("txtTarget").innerText != "..."');
+    const translatedText = await page.evaluate('document.getElementById("txtTarget").innerText');
     await browser.close();
-    return htmlToText.fromString(translatedHtml)
+    return translatedText;
+  } catch (e) {
+    await browser?.close();
+    return undefined;
+  }
 }
+
+module.exports = puppeteerPapagoScraping;
